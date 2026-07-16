cd6e4390ef43b65324f2f5fcf1f316f7d6232605
wajihulqammar <wajiul.qammar@gmail.com>
2026-07-16 17:11:06 +0500

    feat: add daphne for automatic WebSocket runserver support

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',            # ASGI server for WebSockets (Must be first)
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
